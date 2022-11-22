acbe287c7b27c95108b7da2e982d245085810278
Code Rat <michael.oshea1@jcu.edu>
2022-11-22 10:53:40 +1000

    add binary search algo in js

---
+++ javascript/binary-search.js
@@ -0,0 +1,27 @@
+const numArray = [1, 3, 5, 7, 9];
+
+function binarySearch(list, item){
+    let low = 0;
+    let high = numArray.length - 1;
+    let Null = "Can't be found!"
+
+    while(low <= high){
+        let mid = (low + high) / 2;
+        let guess = list[mid];
+
+        if (guess == item)
+        {
+            return mid;
+        }
+        else if(guess > item)
+        {
+            high = mid
+        }
+        else low = mid
+    }
+    return Null
+}
+
+console.log(binarySearch(numArray, 7))
+
+
